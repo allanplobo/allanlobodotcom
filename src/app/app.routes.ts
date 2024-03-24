d175f61e408c4cd45e09476020566e8b23857be1
Allan Lobo <allanplobo@gmail.com>
2024-03-24 20:49:28 -0300

    feat: finish home page, add title and fix route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,26 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full',
-  },
-  {
     path: '',
     component: HomeComponent,
+    title:
+      'Allan Lobo | Serviços em Desenvolvimento de sistemas e Blog técnico',
   },
   {
     path: 'blog',
     loadComponent: () =>
       import('./pages/blog/blog.component').then((mod) => mod.BlogComponent),
+    title: 'Allan Lobo | Blog com conteúdos técnicos sobre programação',
   },
   {
     path: 'sobre',
     loadComponent: () =>
       import('./pages/about/about.component').then((mod) => mod.AboutComponent),
+    title: 'Allan Lobo | Sobre mim',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
