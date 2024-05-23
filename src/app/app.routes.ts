b2b227911ffa8d6f24d45c768179f70e48269dbb
Allan Lobo <allanplobo@gmail.com>
2024-05-23 15:54:49 -0300

    feat: init header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,19 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   {
     path: '',
+    title:
+      'Allan Lobo - Desenvolvedor e criador de conteúdo sobre tecnologia e desenvolvimento.',
     component: HomeComponent,
   },
   {
+    path: 'sobre',
+    title: 'Sobre | Allan Lobo',
+    loadComponent: () =>
+      import('./about/about.component').then((sc) => sc.AboutComponent),
+  },
+  {
     path: 'blog',
+    title: 'Blog | Allan Lobo',
     loadComponent: () =>
       import('./blog/blog.component').then((sc) => sc.BlogComponent),
   },
